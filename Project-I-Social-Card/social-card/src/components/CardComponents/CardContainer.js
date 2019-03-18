be766969be13e2623b23a62cee609c0b7aecd779
benjchand <benchand@gmail.com>
2019-03-18 16:50:25 -0700

    Almost finished with MVP.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,23 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner.js';
+import CardContent from './CardContent.js';
+
+const CardContainer = () => {
+    return (
+        <div className = "card-container" id="card-container">
+            <CardBanner />
+            <CardContent />
+        </div>
+    )
+}
+
+// function openlink() {
+//     window.open("google.com");
+
+// }
+// document.getElementById("card-container").addEventListener("click", openlink);
+
+
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+import HeaderImage from './ImageThumbnail.js';
+import HeaderInfo from './HeaderTitle.js';
+import HeaderContent from './HeaderContent.js';
+
+
+
+const HeaderContainer = () => {
+    return (
+        <div className="header-container header-div">
+            <HeaderImage />
+            <HeaderContent />
+        </div>
+    )
+}
+
+
+
+export default HeaderContainer;
