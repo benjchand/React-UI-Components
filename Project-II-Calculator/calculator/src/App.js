31aabfbff31d37235ae39eef89ec954f5fa5551b
benjchand <benchand@gmail.com>
2019-03-19 16:23:41 -0700

    Calculator styling is complete.

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,37 +3,52 @@ import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton.js';
 import ActionButton from './components/ButtonComponents/ActionButton.js';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js';
+import ButtonRow from './components/ButtonComponents/ButtonRow.js';
 
 const App = () => {
   return (
     <div className = 'main-calculator-div'>
       <CalculatorDisplay />
-      <div className='first-row'>
-        <ActionButton text = 'clear' />
-        <NumberButton text = '&#247;' bgColor = 'red-button'/>
-        
+
+      <div className="first-row">
+        <ButtonRow>
+          <ActionButton text = 'clear' />
+          <NumberButton text = '&#247;' bgColor = 'red-button'/>
+        </ButtonRow >
       </div>
+
       <div className='second-row'>
-        <NumberButton text = '7' bgColor = 'white-button'/>
-        <NumberButton text = '8' bgColor = 'white-button'/>
-        <NumberButton text = '9' bgColor = 'white-button'/>
-        <NumberButton text = '&#215;' bgColor = 'red-button'/>
+        <ButtonRow>
+          <NumberButton text = '7' bgColor = 'white-button'/>
+          <NumberButton text = '8' bgColor = 'white-button'/>
+          <NumberButton text = '9' bgColor = 'white-button'/>
+          <NumberButton text = '&#215;' bgColor = 'red-button'/>
+        </ButtonRow>
       </div>
+
       <div className='third-row'>
-        <NumberButton text = '4' bgColor = 'white-button'/>
-        <NumberButton text = '5' bgColor = 'white-button'/>
-        <NumberButton text = '6' bgColor = 'white-button'/>
-        <NumberButton text = '-' bgColor = 'red-button'/>
+        <ButtonRow>
+          <NumberButton text = '4' bgColor = 'white-button'/>
+          <NumberButton text = '5' bgColor = 'white-button'/>
+          <NumberButton text = '6' bgColor = 'white-button'/>
+          <NumberButton text = '-' bgColor = 'red-button'/>
+        </ButtonRow>
       </div>
+
       <div className='fourth-row'>
-        <NumberButton text = '1' bgColor = 'white-button'/>
-        <NumberButton text = '2' bgColor = 'white-button'/>
-        <NumberButton text = '3' bgColor = 'white-button'/>
-        <NumberButton text = '+' bgColor = 'red-button'/>
+        <ButtonRow>
+          <NumberButton text = '1' bgColor = 'white-button'/>
+          <NumberButton text = '2' bgColor = 'white-button'/>
+          <NumberButton text = '3' bgColor = 'white-button'/>
+          <NumberButton text = '+' bgColor = 'red-button'/>
+        </ButtonRow>
       </div>
+
       <div className='fifth-row'>
-        <ActionButton text = '0' />
-        <NumberButton text = '=' bgColor = 'red-button'/>
+        <ButtonRow>
+          <ActionButton text = '0' />
+          <NumberButton text = '=' bgColor = 'red-button'/>
+        </ButtonRow>
       </div>
     </div>
   );
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonRow.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonRow.js
@@ -4,10 +4,10 @@ import './Button.css';
 
 const ButtonRow = props => {
     return (
-        <div className = {`button-row`}>
+        <div className = 'button-row'>
             {props.children}
         </div>
-    )
+    );
 }
 
 export default ButtonRow;
